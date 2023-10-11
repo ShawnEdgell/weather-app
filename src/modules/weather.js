47738dbbce9b535f4b9b3df4f62c80685ded3243
shawn edgell <shawn_edgell@hotmail.com>
2023-10-11 02:28:46 -0400

    changed api url to https

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -2,7 +2,7 @@ const API_KEY = "219a015ee939448aa0f195825231010";
 
 export async function fetchWeather() {
   const location = document.getElementById("location").value;
-  const url = `http://api.weatherapi.com/v1/current.json?key=219a015ee939448aa0f195825231010&q=${location}`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=219a015ee939448aa0f195825231010&q=${location}`;
   const response = await fetch(url);
 
   if (response.ok) {
